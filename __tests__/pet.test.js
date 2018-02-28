94704739418edbdfc6d2d586c12f6436b719982d
Bradley <b.leadbetter@live.co.uk>
2018-02-28 14:22:31 +0000

    Tamagotchi now gets older by one year

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -13,4 +13,18 @@ describe('constructor', () => {
       expect(pet.name).toEqual('Fido');
     });
 });
-  
+
+describe('constructor', () => {
+    it('the tamagotchi has an intial age of 0', () => {
+        const pet = new Pet('Fido');
+        expect(pet.age).toEqual(0);
+    });
+});
+
+describe('constructor', () => {
+    it('Makes the Tamagotchi one year closer to kicking the bucket', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.age).toEqual(1);
+    });
+});
--- src/pet.js
+++ src/pet.js
@@ -1,8 +1,14 @@
 function Pet() {}
 module.exports = Pet;
+
 function Pet(name) {
-    this.name = name;
+    this.name = 'Fido';
+    this.age = 0;
 }
+
+Pet.prototype.growUp = function() {
+    this.age += 1;
+};
 const pet = {
-    name: 'Fido'
+    name: 'Fido',
 };
